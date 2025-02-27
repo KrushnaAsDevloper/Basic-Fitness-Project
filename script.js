796ac5a8271807192500477ce73958558455698f
KrushnaAsDevloper <krushnawaghmare165@gmail.com>
2025-02-27 18:54:19 +0530

    upgrading

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+
 const animatedText = document.querySelector('.gym-name');
-    const hiddenContent = document.getElementById('content');
+const hiddenContent = document.getElementById('content');
 
-    animatedText.addEventListener('animationend', () => {
-      hiddenContent.classList.add('visible');
-    })
+animatedText.addEventListener('animationend', () => {
+  hiddenContent.classList.add('visible');
+})
 
-  
-
+const sidebar = document.querySelector(".ham").addEventListener("click",()=>{
+document.querySelector(".sidebar").classList.toggle("sidebarGo")
+})
